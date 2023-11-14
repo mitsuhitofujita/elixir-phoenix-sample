a3b8c8515e3c74a24761b3be709adb3b7b275c70
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-11-14 15:19:46 +0000

    Initialize new LiveView project

---
+++ phoenix/02-liveview/config/test.exs
@@ -0,0 +1,30 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :main, Main.Repo,
+  database: Path.expand("../main_test.db", Path.dirname(__ENV__.file)),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :main, MainWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "JCZ9lTNszj7rLX+eQtyjA1VeOSKVd3mXSnVWsIxxK0qhO+lDhy+hjDe55l6bXUY6",
+  server: false
+
+# In test we don't send emails.
+config :main, Main.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
