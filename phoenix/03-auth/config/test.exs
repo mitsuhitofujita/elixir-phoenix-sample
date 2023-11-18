00a96dee287e05afc5ad7360693a14f6202c4b3b
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-11-18 03:26:55 +0000

    Add auth sample project

---
+++ phoenix/03-auth/config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Only in tests, remove the complexity from the password hashing algorithm
+config :bcrypt_elixir, :log_rounds, 1
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :main, Main.Repo,
+  database: Path.expand("../main_test.db", Path.dirname(__ENV__.file)),
+  pool_size: 5,
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :main, MainWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "RK/MALXxJEXceHeuGh4DnyfQqy/cyqkdwHDp73HoC9IrGUfTxPAauGD+iqGl92QK",
+  server: false
+
+# In test we don't send emails.
+config :main, Main.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
